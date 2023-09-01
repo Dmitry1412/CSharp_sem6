c646685535dd888ea8733b1d0afd2a5549940b17
Dmitry <sicily007@rambler.ru>
2023-09-01 21:46:14 +0300

    sem6

---
+++ example_039/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 39: Напишите программу,
+которая перевернёт одномерный массив 
+(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+*/
+var arr = GenerateArray(11);
+PrintArray(arr);
+ReverseArray(arr);
+System.Console.WriteLine();
+PrintArray(arr);
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int tmp = array[i];
+        array[i] = array[array.Length-1-i];
+        array[array.Length-1-i] = tmp;
+    }
+}
+
+int[] GenerateArray(int length)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(0, 100);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int i in array) // перебирает непосредственно значение элементов массива
+    {
+        Console.Write($"{i} ");
+    }
+}
---
+++ example_040/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+Задача 40: Напишите программу, которая принимает на вход три числа и проверяет,
+может ли существовать треугольник с сторонами такой длины.
+*/
+
+System.Console.WriteLine(IsTriangle(1, 1, 9));
+
+bool IsTriangle(int stA, int stB, int stC)
+{
+    return (((stA + stB) > stC) && ((stB + stC) > stA) && ((stA + stC) > stB));
+}
+
+/*
+Решение через лямбда-выражение
+
+var istriangle = (int stA, int stB, int stC) =>
+    (((stA + stB) > stC) && ((stB + stC) > stA) && ((stA + stC) > stB));
+System.Console.WriteLine(istriangle(6, 5, 9));
+*/
+
+
+/*
+/// Работа с классом List - Дополнительная информация
+
+var persons = new List<Person> {
+    new Person {
+        Id = 1, 
+        Name = "Dima"
+    }, 
+    new Person {
+        Id = 2, 
+        Name = "Misha"
+    },
+};
+persons.Add(new Person {
+        Id = 3, 
+        Name = "Vasya"
+    });
+System.Console.WriteLine(persons.Where(p => p.Id == 3).FirstOrDefault()?.Name); // p => p.Id дямбда-выражение (делегат)
+System.Console.WriteLine(persons[1].Name);
+public class Person
+{
+    public int Id {get; set;}
+    public string Name {get; set;}
+}
+*/
+
+
+/// https://www.youtube.com/watch?v=rPp46idEvnM&t=1s
+/// Хеш-таблица — Самая Популярная Структура Данных
+/// полезное видео
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
